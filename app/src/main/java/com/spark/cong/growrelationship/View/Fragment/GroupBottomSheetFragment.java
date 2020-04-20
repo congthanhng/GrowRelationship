c0e1a81318b2a8ef42ff63c4c4354bee4e532847
nhocchoda <nhocchoda@gmail.com>
2020-04-20 17:45:33 +0700

    add GroupBottomSheetFragment

---
+++ app/src/main/java/com/spark/cong/growrelationship/View/Fragment/GroupBottomSheetFragment.java
@@ -0,0 +1,69 @@
+package com.spark.cong.growrelationship.View.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.spark.cong.growrelationship.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GroupBottomSheetFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GroupBottomSheetFragment extends BottomSheetDialogFragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+//    private String mParam1;
+//    private String mParam2;
+
+    public GroupBottomSheetFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment GroupBottomSheetFragement.
+     */
+    // TODO: Rename and change types and number of parameters
+//    public static GroupBottomSheetFragement newInstance(String param1, String param2) {
+//        GroupBottomSheetFragement fragment = new GroupBottomSheetFragement();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+//        TextView textView = new TextView(getActivity());
+//        textView.setText(R.string.hello_blank_fragment);
+        View v = inflater.inflate(R.layout.fragment_bottom_sheet_dialog_group,container,false);
+
+        return v;
+    }
+}
